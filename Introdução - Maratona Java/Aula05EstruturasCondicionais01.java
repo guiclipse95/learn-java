4ad1478752d3b539e722a725129101bb613c9e21
Guilherme Nascimento <116480912+guiclipse95@users.noreply.github.com>
2025-04-11 10:49:54 -0300

    Add files via upload

---
+++ Introdução - Maratona Java/Aula05EstruturasCondicionais01.java
@@ -0,0 +1,17 @@
+package com.guiclipse.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais01 {
+
+    public static void main(String[] args) {
+
+        int idade = 15;
+
+        if (idade >= 18) {
+            System.out.println("Autorizado a beber bebida alcólica");
+        } else{
+            System.out.println("Não autorizado a beber bebida alcólica");
+        }
+        
+
+    }
+}
